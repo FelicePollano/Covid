ef5e03a12d87bdc90cffc1d7703c43a692889689
Felice Pollano <felice@felicepollano.com>
2020-04-04 12:05:23 +0200

    extracting data

--- src/CovidIndexer/DataDoc.cs
+++ src/CovidIndexer/DataDoc.cs
@@ -1,10 +1,17 @@
 using System;
+using Nest;
 
 namespace CovidIndexer
 {
     class DataDoc
     {
+     
         public DateTime TimeStamp {get;set;}
-
+       
+        public string ProvinceState { get; set; }
+        public string CountryRegion { get; set; }
+       
+        public double Value { get; set; }
+        public GeoLocation Location { get; set; }
     }
 }
--- src/CovidIndexer/Program.cs
+++ src/CovidIndexer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Nest;
 
 namespace CovidIndexer
 {
@@ -10,17 +12,36 @@ namespace CovidIndexer
     {
         static async Task Main(string[] args)
         {
+
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200"));
+            
+
+
             HttpClient client = new HttpClient();
             var stream = await client.GetStreamAsync("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv");
             string line;
+            
+            var esclient = new ElasticClient(settings);
+
             
             using(var sr = new StreamReader(stream))
             {
                 line = await sr.ReadLineAsync();
                 var dates = ExtractTimeStamps(line);
+                
                 while(null!=(line = await sr.ReadLineAsync()))
                 {
-                    Console.WriteLine(line);
+                    var tokens = line.Split(',');
+                    foreach(var one in dates)
+                    {
+                        one.Location = new Nest.GeoLocation(double.Parse(tokens[2],CultureInfo.InvariantCulture),double.Parse(tokens[3],CultureInfo.InvariantCulture));
+                        one.CountryRegion = tokens[1].Trim('\"');
+                        one.ProvinceState = tokens[0].Trim('\"');
+                    }
+                    for(int i=4;i<tokens.Length;i++)
+                    {
+                        dates[i-4].Value = double.Parse(tokens[i],CultureInfo.InvariantCulture);
+                    }
                 }
             }
             
